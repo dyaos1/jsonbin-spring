ec33152c20a1cd27e8e0e48ed4a61f4328f0bebe
Flzomb <spark3840@gmail.com>
2024-09-21 19:00:35 +0900

    6-1)minor fix: setting redisTemplate

--- src/main/kotlin/be/dev/jsonbin/config/RedisConfig.kt
+++ src/main/kotlin/be/dev/jsonbin/config/RedisConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
@@ -12,6 +13,8 @@ class RedisConfig {
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
         val template = RedisTemplate<String, String>()
         template.connectionFactory = redisConnectionFactory
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = StringRedisSerializer()
         return template
     }
 }
